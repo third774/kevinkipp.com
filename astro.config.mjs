5aef17ae356b44d65ed228854cb031ecb6699bdd
Kevin Kipp <kevin.kipp@gmail.com>
2023-09-09 15:30:52 -0500

    Fix iOS tap flash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kevinkipp.com',
-	integrations: [mdx(), sitemap(), tailwind()],
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
