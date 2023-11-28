67b2a11dae7b8e2e8b35b7a2a8b2540a217927a3
Kevin Kipp <kevin.kipp@gmail.com>
2023-11-28 15:37:05 -0600

    Move post sorting into utility

--- src/content/blog/_getPublishedBlogPosts.ts
+++ src/content/blog/_getPublishedBlogPosts.ts
@@ -2,7 +2,12 @@ import { getCollection } from "astro:content";
 
 export const getPublishedBlogPosts = async () => {
 	const blog = await getCollection("blog");
-	return blog.filter((b) =>
-		import.meta.env.PROD ? b.data.published > new Date(0) : true,
+	return (
+		blog
+			.filter((blogPost) =>
+				import.meta.env.PROD ? blogPost.data.published > new Date(0) : true,
+			)
+			// sort by published date putting the most recent first
+			.sort((a, b) => b.data.published.getTime() - a.data.published.getTime())
 	);
 };
