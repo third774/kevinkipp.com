d8f3e8821345ca6ca3cce60e45cadb025940fb36
Kevin Kipp <kevin.kipp@gmail.com>
2023-12-23 15:39:02 -0600

    update og images

--- src/utils/ogImages/generateBlogPostOgImage.ts
+++ src/utils/ogImages/generateBlogPostOgImage.ts
@@ -15,6 +15,11 @@ await page.setViewport({
 	deviceScaleFactor: 1,
 });
 
+// Load profile picture into memory
+const profilePicture = fs.readFileSync("src/assets/profile.jpg");
+
+const base64ProfilePicture = Buffer.from(profilePicture).toString("base64");
+
 const template = (props: { title: string; description?: string }) => `
 <!DOCTYPE html>
 <html lang="en">
@@ -29,7 +34,7 @@ const template = (props: { title: string; description?: string }) => `
 				--foreground: #E1E3EE;
 				--dim: #B7BAC4;
 				font-family: 'Recursive', 'Noto Color Emoji', sans-serif;
-				font-size: 2.5rem;
+				font-size: 2rem;
 				line-height: 1.5;
 			}
 
@@ -48,7 +53,7 @@ const template = (props: { title: string; description?: string }) => `
 			}
 
 			h1 {
-				font-size: 2rem;
+				font-size: 1.75rem;
 				font-weight: 1000;
 				text-wrap: balance;
 				line-height: 1;
@@ -59,7 +64,6 @@ const template = (props: { title: string; description?: string }) => `
 			}
 
 			p {
-				padding-top: 1em;
 				text-wrap: pretty;
 			}
 
@@ -68,14 +72,24 @@ const template = (props: { title: string; description?: string }) => `
 			}
 
 			.grid {
-				padding: 1rem;
+				padding: 2.5rem;
 				display: grid;
 				align-content: center;
+				gap: 1.5rem;
+			}
+
+			.name {
+				text-decoration: underline;
+				text-decoration-color: oklch(.744141 .151562 340 / 1);
 			}
 		</style>
   </head>
   <body>
 		<div class="grid">
+			<div style="display: flex; align-items: center;">
+				<img style="display: block; border-radius: 35% 65% 60% 25% / 35% 20% 60% 65%; max-width: 3rem;" src="data:image/png;base64, ${base64ProfilePicture}" />
+				<p style="opacity: .7; margin-left: 1rem;">by <span class="name">Kevin Kipp</span></p>
+			</div>
 			<h1>${props.title}</h1>
 			${props.description ? `<p class="dim">${props.description}</p>` : ""}
 		</div>
@@ -87,9 +101,9 @@ export async function generateBlogPostOgImage(post: CollectionEntry<"blog">) {
 	const path = `${isProd ? "dist" : "public"}/open-graph/`;
 
 	// if file exists, skip
-	if (fs.existsSync(`${path}${filename}`)) {
-		return;
-	}
+	// if (fs.existsSync(`${path}${filename}`)) {
+	// 	return;
+	// }
 
 	await page.setContent(template(post.data));
 	await page.waitForNetworkIdle();
