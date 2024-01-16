ede27a686424eeda05eecfd13cf52189f678fca4
Kevin Kipp <kkipp@cloudflare.com>
2024-01-16 12:06:41 -0600

    Add redirect links

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,6 @@
 export const SITE_TITLE = "Kevin Kipp";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const TWITTER_PROFILE = "https://twitter.com/kevin_kipp";
-export const MASTADON_PROFILE = "https://hachyderm.io/@third774";
+export const MASTODON_PROFILE = "https://hachyderm.io/@third774";
 export const GITHUB_PROFILE = "https://github.com/third774";
+export const CALCOM_30_MINUTES_LINK = "https://cal.com/kkipp/30min";
