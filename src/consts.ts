751486520d24408bdcd2b8ec32011b5cc6f2f998
Kevin Kipp <kevin.kipp@gmail.com>
2024-03-03 14:23:14 -0600

    Update SITE_DESCRIPTION in consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Kevin Kipp";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "UI Engineer based in Austin, TX.";
 export const TWITTER_PROFILE = "https://twitter.com/kevin_kipp";
 export const MASTODON_PROFILE = "https://hachyderm.io/@third774";
 export const GITHUB_PROFILE = "https://github.com/third774";
