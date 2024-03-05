d1bd67ca871809d6719f70b71800d48e4358f670
Kevin Kipp <kevin.kipp@gmail.com>
2024-03-04 22:19:57 -0600

    Add db:studio support

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,12 +3,20 @@ import path from "path";
 
 const wranglerConfigPath = path.resolve(__dirname, "wrangler.toml");
 
-export default {
-	schema: "./src/schema.ts",
-	out: "./migrations",
-	driver: "d1",
-	dbCredentials: {
-		dbName: "kevinkipp.com",
-		wranglerConfigPath,
-	},
-} satisfies Config;
+export default process.env.LOCAL_DB_PATH
+	? ({
+			schema: "./src/schema.ts",
+			driver: "better-sqlite",
+			dbCredentials: {
+				url: process.env.LOCAL_DB_PATH!,
+			},
+		} satisfies Config)
+	: ({
+			schema: "./src/schema.ts",
+			out: "./migrations",
+			driver: "d1",
+			dbCredentials: {
+				wranglerConfigPath,
+				dbName: "kevinkipp.com",
+			},
+		} satisfies Config);
