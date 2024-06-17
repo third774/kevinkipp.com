3ebe7c9f4e2b7452bc6f854b68a741121fa1a793
Kevin Kipp <kevin.kipp@gmail.com>
2024-06-17 00:09:56 -0500

    Add bsky social link

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,6 @@ export const SITE_TITLE = "Kevin Kipp";
 export const SITE_DESCRIPTION = "UI Engineer based in Austin, TX.";
 export const TWITTER_PROFILE = "https://twitter.com/kevin_kipp";
 export const MASTODON_PROFILE = "https://hachyderm.io/@third774";
+export const BLUESKY_PROFILE = "https://bsky.app/profile/kkipp.dev";
 export const GITHUB_PROFILE = "https://github.com/third774";
 export const CALCOM_30_MINUTES_LINK = "https://cal.com/kkipp/30min";
