abed68a80f78262d6932a573dd83111fe7a136b1
Kevin Kipp <kevin.kipp@gmail.com>
2024-10-05 14:37:13 -0500

    Update Astro

--- astro.config.ts
+++ astro.config.ts
@@ -3,12 +3,34 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://kevinkipp.com",
+	experimental: {
+		env: {
+			schema: {
+				FEEDBIN_EMAIL: envField.string({ access: "secret", context: "server" }),
+				FEEDBIN_PASSWORD: envField.string({
+					access: "secret",
+					context: "server",
+				}),
+				API_TOKEN: envField.string({ access: "secret", context: "server" }),
+				BSKY_LOGIN: envField.string({
+					access: "secret",
+					context: "server",
+					optional: true,
+				}),
+				BSKY_PASSWORD: envField.string({
+					access: "secret",
+					context: "server",
+					optional: true,
+				}),
+			},
+		},
+	},
 	integrations: [
 		mdx(),
 		sitemap(),
