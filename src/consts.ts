f66c1b2716041c4b2c56bd63163cd562dc05024e
Kevin Kipp <kkipp@cloudflare.com>
2025-01-24 14:32:57 -0600

    Remove twitter stuff

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,6 @@
 
 export const SITE_TITLE = "Kevin Kipp";
 export const SITE_DESCRIPTION = "UI Engineer based in Austin, TX.";
-export const TWITTER_PROFILE = "https://twitter.com/kevin_kipp";
 export const MASTODON_PROFILE = "https://hachyderm.io/@third774";
 export const BLUESKY_PROFILE = "https://bsky.app/profile/kkipp.dev";
 export const GITHUB_PROFILE = "https://github.com/third774";
