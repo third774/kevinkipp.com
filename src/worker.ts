4163dbfbc3006b0ccf97f5297b049f98ed85616d
Kevin Kipp <kevin.kipp@gmail.com>
2025-07-23 01:30:42 -0500

    Fix formatting

--- src/worker.ts
+++ src/worker.ts
@@ -1,21 +1,22 @@
-import type { SSRManifest } from 'astro';
-import { App } from 'astro/app';
+import type { SSRManifest } from "astro";
+import { App } from "astro/app";
 
 export function createExports(manifest: SSRManifest) {
-  const app = new App(manifest);
-  
-  return {
-    default: {
-      async fetch(request: Request, env: Env, ctx: ExecutionContext) {
-        return app.render(request, {
-          locals: {
-            runtime: {
-              env,
-              ctx
-            }
-          }
-        });
-      }
-    } satisfies ExportedHandler<Env>
-  };
+	const app = new App(manifest);
+
+	return {
+		default: {
+			async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+				return app.render(request, {
+					locals: {
+						runtime: {
+							env,
+							ctx,
+						},
+					},
+				});
+			},
+		} satisfies ExportedHandler<Env>,
+	};
 }
+
